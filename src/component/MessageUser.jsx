b1dd3ae64a0ac4a2d3d60e280bb0d708b2483f75
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-08-25 15:51:53 +0430

    update style message users

--- src/component/MessageUser.jsx
+++ src/component/MessageUser.jsx
@@ -1,14 +1,25 @@
-import { useContext } from 'react';
+import { useContext, useEffect ,useState} from 'react';
 import { Store } from '../context/Store';
 import { TiTickOutline } from 'react-icons/ti'
 
 function MessageUser() {
   const { state } = useContext(Store);
   const { message, userinfo } = state;
+  const [sync,setSync]=useState(false)
+   const [moveSlider, setMoveSlider] = useState({
+     move: 'transform translate-x-[1000px]',
+   });
   const filterMessage = message.filter(
     (f) => f.user_id._id === userinfo[0]?._id
   );
-
+  useEffect(() => {
+     setMoveSlider('transform translate-x-[0px]');
+  }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      setSync(true)
+    },1800)
+  },[])
   return (
     <div className="flex items-center flex-col max-h-screen overflow-y-auto">
       <div className=" p-10 flex flex-col gap-5 bg-zinc-700 text-yellow-200  text-2xl ">
@@ -16,10 +27,12 @@ function MessageUser() {
         {filterMessage.map((m) => (
           <div
             key={m._id}
-            className="flex justify-between items-center mt-5 gap-[0px] shadow shadow-yellow-100 p-5 "
+            className={`flex justify-between items-center ${moveSlider.move} duration-1000 rounded-lg mt-5 gap-[0px] shadow shadow-yellow-100 p-5 `}
           >
-            <p>{m.message}</p>
-            <TiTickOutline className="text-green-500" />
+            <div className="flex flex-col gap-5 ">
+              <p className=" ">{m.message}</p>
+              <TiTickOutline className={`text-green-500 ${sync ? 'block':"hidden"} duration-[2000ms] `} />
+            </div>
           </div>
         ))}
       </div>
