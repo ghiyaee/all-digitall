6d1de66b8bf07faa85c9eebc594899b338c6e05e
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-08-27 15:22:07 +0430

    update component address

--- src/component/AddressUsers.jsx
+++ src/component/AddressUsers.jsx
@@ -8,13 +8,16 @@ function AddressUsers() {
   const [postCode, setPostCode] = useState('');
   const [mobile, setMobile] = useState('');
   const [tell, setTell] = useState('');
+  const handelAddress = (e) => {
+    e.preventDefault()
+  }
 
   return (
     <div className="flex flex-col  gap-4 p-10  bg-zinc-700 container m-auto">
       <h2 className="text-yellow-200 text-2xl text-center">
         اطلاعات تکمیلی (آدرس)
       </h2>
-      <form className="flex  flex-col text-2xl gap-5 w-96 font-[yekan]">
+      <form className="flex  flex-col text-2xl gap-5 w-96 font-[yekan]" onSubmit={handelAddress}>
         <input
         onChange={(e)=>setProvince(e.target.value.toLowerCase())}
         value={province}
@@ -61,7 +64,7 @@ function AddressUsers() {
           className="text-yellow-200 py-4 w-[190px] rounded-lg 
         hover:scale-95 duration-300 cursor-pointer bg-zinc-900
         border-r-[3px] border-l-[3px]
-         hover:text-red-500    hover:border-red-500"
+         hover:text-yellow-500    hover:border-red-500"
         >
           ثبت
         </button>
