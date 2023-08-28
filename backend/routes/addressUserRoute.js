f94fe70a7aba4bb1a3001b7418b6e5a92ffaf466
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-08-28 16:34:27 +0430

    update address component

--- backend/routes/addressUserRoute.js
+++ backend/routes/addressUserRoute.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import Address from '../models/addressUsersModel.js';
+import { ObjectId } from 'mongodb';
 const addressRouter = express.Router();
 addressRouter.post('/', async (req, res) => {
   const address = await Address.find({ user_id: req.body.userinfo[0]._id });
+  // const address = await Address.findOneAndUpdate({ user_id:new ObjectId( req.body.userinfo[0]._id), isAddress: false }, { isAddress: true });
   res.send(address);
+  console.log(address);
 });
 
 addressRouter.post('/addressNew', async (req, res) => {
@@ -15,24 +18,20 @@ addressRouter.post('/addressNew', async (req, res) => {
     mobile: req.body.mobile,
     tell: req.body.tell,
     user_id: req.body.userinfo[0]._id,
-    isAddress:true
   });
-  await address.save();
+  await address.save()
 });
 export default addressRouter;
 
-// addressRouter.post('/edit', async (req, res) => {
-//   const address = await Address.findOneAndUpdate(
-//     { user_id: req.body.userinfo[0]._id},
-//     {
-//       province: req.body.province,
-//       city: req.body.city,
-//       street: req.body.street,
-//       postCode: req.body.postCode,
-//       mobile: req.body.mobil,
-//       tell: req.body.tell,
-//     },
-//     {new:true}
-//   );
-//   console.log(address);
-// });
+addressRouter.post('/edit', async (req, res) => {
+  const address = await Address.findOneAndUpdate({
+    user_id: new ObjectId(req.body.userinfo[0]._id),
+  });
+  address.province = req.body.province;
+  address.city = req.body.city;
+  address.street = req.body.street;
+  address.postCode = req.body.postCode;
+  address.mobile = req.body.mobile;
+  address.tell = req.body.tell;
+  await address.save();
+});
--- src/component/AddressUsers.jsx
+++ src/component/AddressUsers.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useState } from 'react';
 import { useContext } from 'react';
-import {Store} from '../context/Store'
+import { Store } from '../context/Store';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 function AddressUsers() {
-  const navigate = useNavigate()
-  const { state } = useContext(Store)
+  const navigate = useNavigate();
+  const { state } = useContext(Store);
   const { userinfo } = state;
   const [province, setProvince] = useState();
   const [city, setCity] = useState();
@@ -17,16 +17,8 @@ function AddressUsers() {
   const [tell, setTell] = useState();
   const [isAddress, setIsAddress] = useState();
   const handelAddress = async (e) => {
-    e.preventDefault()
-    if (!isAddress) {
+    e.preventDefault();
       const fetchData = await axios.post('/api/address/addressNew', {
-        userinfo,
-        province, city, street, postCode, mobile, tell
-      });
-      navigate('/CheckOut');
-   
-    } else {
-      const fetchData = await axios.post('/api/address/edit', {
         userinfo,
         province,
         city,
@@ -35,91 +27,107 @@ function AddressUsers() {
         mobile,
         tell,
       });
-      navigate('/CheckOut');
-    
+      navigate('/CheckOut') 
     }
+  const handelEdit =async () => {
+    const fetchData = await axios.post('/api/address/edit', {
+      userinfo,
+      province,
+      city,
+      street,
+      postCode,
+      mobile,
+      tell,
+    })
+     navigate('/CheckOut');
   }
-    useEffect(() => {
-      const fetchData = async () => {
-        const result = await axios.post('/api/address', { userinfo })
-        setProvince(result.data[0].province)
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.post('/api/address', { userinfo });
+      if (result.data !== '') {
+        setProvince(result.data[0].province);
         setCity(result.data[0].city);
         setMobile(result.data[0].mobile);
         setPostCode(result.data[0].postCode);
         setStreet(result.data[0].street);
         setTell(result.data[0].tell);
+        setIsAddress(result.data[0].isAddress);
+      } else {
+        return;
       }
-      fetchData()
-    }, [])
-    return (
-      <div className="flex flex-col  gap-4 p-10  bg-zinc-700 container m-auto">
-        <h2 className="text-yellow-200 text-2xl text-center">
-          اطلاعات تکمیلی (آدرس)
-        </h2>
-        <form
-          className="flex  flex-col text-2xl gap-5 w-96 font-[yekan]"
-          onSubmit={handelAddress}
-        >
-          <input
-            onChange={(e) => setProvince(e.target.value.toLowerCase())}
-            value={province}
-            type="text"
-            className="py-3 px-1 rounded-lg w-[190px] outline-none"
-            placeholder="استان"
-          />
-          <input
-            onChange={(e) => setCity(e.target.value.toLowerCase())}
-            value={city}
-            type="text"
-            className="py-3 px-1 rounded-lg w-[190px] outline-none"
-            placeholder="شهر"
-          />
-          <input
-            onChange={(e) => setStreet(e.target.value.toLowerCase())}
-            value={street}
-            type="text"
-            className="py-3 px-1 rounded-lg w-[1200px] outline-none"
-            placeholder="خیابان"
-          />
-          <input
-            onChange={(e) => setPostCode(e.target.value.toLowerCase())}
-            value={postCode}
-            type="text"
-            className="py-3 px-1 rounded-lg w-[190px] outline-none"
-            placeholder="کدپستی"
-          />
-          <input
-            onChange={(e) => setMobile(e.target.value.toLowerCase())}
-            value={mobile}
-            type="text"
-            className="py-3 px-1 rounded-lg w-[190px] outline-none"
-            placeholder="موبایل"
-          />
-          <input
-            onChange={(e) => setTell(e.target.value.toLowerCase())}
-            value={tell}
-            type="text"
-            className="py-3 px-1 rounded-lg w-[190px] outline-none"
-            placeholder="تلفن ثابت"
-          />
-          <button
-            className="text-yellow-200 py-4 w-[190px] rounded-lg 
+    };
+    fetchData();
+  }, []);
+  console.log(isAddress);
+  return (
+    <div className="flex flex-col  gap-4 p-10  bg-zinc-700 container m-auto">
+      <h2 className="text-yellow-200 text-2xl text-center">
+        اطلاعات تکمیلی (آدرس)
+      </h2>
+      <form
+        className="flex  flex-col text-2xl gap-5 w-96 font-[yekan]"
+        onSubmit={handelAddress}
+      >
+        <input
+          onChange={(e) => setProvince(e.target.value.toLowerCase())}
+          value={province}
+          type="text"
+          className="py-3 px-1 rounded-lg w-[190px] outline-none"
+          placeholder="استان"
+        />
+        <input
+          onChange={(e) => setCity(e.target.value.toLowerCase())}
+          value={city}
+          type="text"
+          className="py-3 px-1 rounded-lg w-[190px] outline-none"
+          placeholder="شهر"
+        />
+        <input
+          onChange={(e) => setStreet(e.target.value.toLowerCase())}
+          value={street}
+          type="text"
+          className="py-3 px-1 rounded-lg w-[1200px] outline-none"
+          placeholder="خیابان"
+        />
+        <input
+          onChange={(e) => setPostCode(e.target.value.toLowerCase())}
+          value={postCode}
+          type="text"
+          className="py-3 px-1 rounded-lg w-[190px] outline-none"
+          placeholder="کدپستی"
+        />
+        <input
+          onChange={(e) => setMobile(e.target.value.toLowerCase())}
+          value={mobile}
+          type="text"
+          className="py-3 px-1 rounded-lg w-[190px] outline-none"
+          placeholder="موبایل"
+        />
+        <input
+          onChange={(e) => setTell(e.target.value.toLowerCase())}
+          value={tell}
+          type="text"
+          className="py-3 px-1 rounded-lg w-[190px] outline-none"
+          placeholder="تلفن ثابت"
+        />
+      </form>
+        <button
+          className="text-yellow-200 py-4 w-[190px] rounded-lg 
         hover:scale-95 duration-300 cursor-pointer bg-zinc-900
         border-r-[3px] border-l-[3px]
          hover:text-yellow-500    hover:border-red-500"
-          >
-            آدرس جدید
-          </button>
-          {/* <button
+        >
+          آدرس جدید
+        </button>
+        <button
           className="text-yellow-200 py-4 w-[190px] rounded-lg 
         hover:scale-95 duration-300 cursor-pointer bg-zinc-900
         border-r-[3px] border-l-[3px]
          hover:text-yellow-500    hover:border-red-500"
         onClick={handelEdit} >
           ویرایش آدرس
-        </button> */}
-        </form>
-      </div>
-    );
-  }
+        </button>
+    </div>
+  );
+}
 export default AddressUsers;
