53c5024a790eb4c9b4bae3e82db357a1af619aee
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-09-02 00:19:46 +0430

    update checkout

--- src/component/CheckOut.jsx
+++ src/component/CheckOut.jsx
@@ -1,17 +1,19 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
+import { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
 function CheckOut() {
   const { state } = useLocation()
- 
+  const [order,setOrder]=useState()
   useEffect(() => {
     const fetchData = async () => {
       const res = await axios.post('/api/order', { state})
-      console.log(res.data);
+      setOrder(res.data);
     }
     fetchData()
-  },[])
+  }, [])
+  console.log(order);
   return (
     <div className='flex justify-center text-2xl font-[yekan]'
     >در حال آماده سازی درگاه پرداخت
