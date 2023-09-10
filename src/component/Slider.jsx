3de50f8e3f5be7c05b8a93a9ae6a7a44d20d235d
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2023-09-10 18:20:55 +0430

    update style slider

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import axios from 'axios';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 function Slider() {
- const [moveSlider, setMoveSlider] = useState({
-   move: 'transform translate-x-[0rem]',
-   move1:'bg-yellow-500'
- });
+  const [moveSlider, setMoveSlider] = useState({
+    move: 'transform translate-x-[0rem]',
+    move1: 'transform translate-x-[0rem]',
+  });
+  // let transformCN = useMemo(() => {
+  //   return moveSlider ? moveSlider.move : moveSlider.move1;
+  // }, [moveSlider]);
      const [slider, setSlider] = useState([]);
      useEffect(() => {
        const fetchData = async () => {
@@ -36,18 +40,42 @@ function Slider() {
          clearInterval(timer);
        };
      }, [moveSlider.move]);
+    // useEffect(() => {
+    //   const timer = setInterval(() => {
+    //     if (moveSlider.move1 === 'transform translate-x-[0rem]') {
+    //       setMoveSlider({ move1: 'transform translate-x-[30rem]' });
+    //     }
+    //     if (moveSlider.move1 === 'transform translate-x-[84rem]') {
+    //       setMoveSlider({ 1: 'transform translate-x-[168rem]' });
+    //     }
+    //     if (moveSlider.move1 === 'transform translate-x-[168rem]') {
+    //       setMoveSlider({ move1: 'transform translate-x-[252rem]' });
+    //     }
+    //     if (moveSlider.move1 === 'transform translate-x-[252rem]') {
+    //       setMoveSlider({ move1: 'transform translate-x-[336rem]' });
+    //     }
+    //     if (moveSlider.move1 === 'transform translate-x-[336rem]') {
+    //       setMoveSlider({ move1: 'transform translate-x-[0rem]' });
+    //     }
+    //   }, 5000);
+    //   return () => {
+    //     clearInterval(timer);
+    //   };
+    // }, [moveSlider.move1]);
+
   return (
     <div className="hidden md:inline-flex ">
-      <div className=" flex flex-1 overflow-hidden gap-[65px]">
+      <div className=" flex flex-1 overflow-hidden gap-5 md:gap-[65px]">
         {slider.map((product) => (
-          <div key={product.id}
-            className={` flex gap-[200px] items-center bg-gradient-to-b 
-             from-green-700 to-yellow-200 w-full p-10 rounded-xl  ${moveSlider.move} duration-1000`}
+          <div
+            key={product.id}
+            className={` flex gap-[50px]  md:gap-[200px] items-center bg-gradient-to-b 
+             from-green-700 to-yellow-200 w-[100%] md:w-full py-2 md:p-6 mx-0 md:rounded-xl ${moveSlider.move}  duration-[2000ms]`}
           >
-            <h1 className="text-zinc-800 text-6xl flex-1 w-[700px] font-[sogand] text-center ">
+            <h1 className="text-zinc-800 text-xl md:text-6xl flex-1 w-[300px] md:w-[700px] font-[sogand] text-center md:text-center ">
               {product.desc}
             </h1>
-            <div className="flex flex-1 justify-center w-[700px] ">
+            <div className="flex flex-1 justify-center w-[400px] md:w-[700px] ">
               <img src={product.img} alt="images" className="imgTop" />
             </div>
           </div>
